3f9009f889d6aa93447fc9afd16cfb9ecbb388e0
ryud2 <114495007+ryud2@users.noreply.github.com>
2024-12-04 09:35:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,15 @@
+A , B , C = map(int,input().split())
+if A == B == C :
+    print(10000 + A*1000)
+elif A == B != C :
+    print(1000 + A*100)
+elif A == C != B :
+    print(1000 + A*100)
+elif B == C != A :
+    print(1000 + B*100)
+elif A > B and A > C:
+    print(A*100)
+elif B > A and B > C:
+    print(B*100)
+elif C > A and C > B:
+    print(C*100)
