2e00b46be4bd6993e3244a0c2a89e929772e1fc7
ryud2 <114495007+ryud2@users.noreply.github.com>
2024-12-11 08:38:34 +0900

    Update 최소공배수.py

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -4,3 +4,17 @@ T = int(sys.stdin.readline().strip())
 for i in range(T):
     A , B = map(int,sys.stdin.readline().split())
     print(math.lcm(A,B))
+
+
+# import sys
+# T = int(sys.stdin.readline())
+# for i in range(T):
+#     A , B = map(int,sys.stdin.readline().split())
+#     a , b = A , B
+#     if A < B :
+#             A , B = B , A
+#     while A%B != 0 :
+#         if A < B :
+#             A , B = B , A
+#         A , B = B , A%B
+#     print(a*b//B)
