30397ff91d393e265a101b553eebf2d87eade0e9
ryud2 <114495007+ryud2@users.noreply.github.com>
2024-12-11 08:43:52 +0900

    [Silver V] Title: 최소공배수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,4 +1,10 @@
-import math
 import sys
 A , B = map(int,sys.stdin.readline().split())
-print(math.lcm(A,B))
+a , b = A , B
+if A < B :
+        A , B = B , A
+while A%B != 0 :
+    if A < B :
+        A , B = B , A
+    A , B = B , A%B
+print(a*b//B)
