182c272e86f20b0260b994fa21b3fe2e6a0903cb
ryud2 <114495007+ryud2@users.noreply.github.com>
2024-12-26 10:56:37 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,38 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+S = []
+
+for i in range(N):
+    
+    command = list(map(str,sys.stdin.readline().split()))
+    
+    if command[0] == "push" :
+        S.append(command[1])
+        
+    if command[0] == "pop":
+        if len(S) == 0:
+            print(-1)
+        else :
+            print(S.pop(0))
+            
+    if command[0] == "size":
+        print(len(S))
+        
+    if command[0] == "empty":
+        if len(S) == 0:
+            print(1)
+        else :
+            print(0)
+    
+    if command[0] == "front":
+        if len(S) == 0:
+            print(-1)
+        else :
+            print(S[0])
+    if command[0] == "back":
+        if len(S) == 0:
+            print(-1)
+        else :
+            print(S[-1])
