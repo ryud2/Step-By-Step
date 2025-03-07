dd712c083c7d02a8fa9a65b3de05e8073672a2be
ryud2 <114495007+ryud2@users.noreply.github.com>
2025-03-07 13:22:28 +0900

    [Bronze IV] Title: 준영이의 등급, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30008. 준영이의 등급/준영이의 등급.py
+++ 백준/Bronze/30008. 준영이의 등급/준영이의 등급.py
@@ -2,30 +2,15 @@ import sys
 
 D = 0
 S = []
-
+C = [4,11,23,40,60,77,89,96,100]
 N , K = map(int,sys.stdin.readline().split())
 
 G = list(map(int,sys.stdin.readline().split()))
 
 for i in range(K):
     D = int( G[i]/N *100 )
-    if D <= 4 :
-        S.append(1)
-    elif D <= 11 :
-        S.append(2)
-    elif D <= 23 :
-        S.append(3)
-    elif D <= 40 :
-        S.append(4)
-    elif D <= 60 :
-        S.append(5)
-    elif D <= 77 :
-        S.append(6)
-    elif D <= 89 :
-        S.append(7)
-    elif D <= 96 :
-        S.append(8)
-    else :
-        S.append(9)
-
+    for j in range(9):
+        if D <= C[j]:
+            S.append(j+1)
+            break
 print(*S)
