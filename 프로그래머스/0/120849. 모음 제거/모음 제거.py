efbb9da28799db0c72f083e95a3772d42291d4a5
ryud2 <114495007+ryud2@users.noreply.github.com>
2025-03-11 17:58:24 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,7 @@
+def solution(my_string):
+    my_string = my_string.replace("a","")
+    my_string = my_string.replace("e","")
+    my_string = my_string.replace("i","")
+    my_string = my_string.replace("u","")
+    my_string = my_string.replace("o","")
+    return my_string
