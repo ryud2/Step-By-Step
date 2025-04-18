4055acb1049754298ec06cb0ad2312dc6708206c
ryud2 <114495007+ryud2@users.noreply.github.com>
2025-04-18 14:16:18 +0900

    [level 0] Title: 소인수분해, Time: 0.91 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    i = 2
+    while n >= i :
+        if n % i == 0 :
+            n = n//i
+            answer.append(i)
+        else:
+            i += 1
+    answer = list(set(answer))
+    return sorted(answer)
