ba0db9a77f5f4fea9043f520d178f388332d9f9c
ryud2 <114495007+ryud2@users.noreply.github.com>
2025-05-23 14:12:32 +0900

    [level 0] Title: 코드 처리하기, Time: 20.91 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,17 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    for i in range(len(code)):
+        if mode == 0 :
+            if code[i] == "1":
+                mode = 1
+            else:
+                if i % 2 == 0 :
+                    answer += code[i]
+        else:
+            if code[i] == "1":
+                mode = 0
+            else:
+                if i % 2 == 1:
+                    answer += code[i]
+    return answer if answer != "" else "EMPTY"
